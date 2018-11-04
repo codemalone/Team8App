c6e204524b5a786f2de35f4394ba6147a8bdaf7b
Jared Malone <jaredmm@uw.edu>
2018-11-04 09:57:36 -0800

    added chat fragment

---
+++ app/src/main/java/tcss450/uw/edu/team8app/ChatFragment.java
@@ -0,0 +1,102 @@
+package tcss450.uw.edu.team8app;
+
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import tcss450.uw.edu.team8app.utils.SendPostAsyncTask;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ChatFragment extends Fragment {
+
+    private static final String TAG = "CHAT_FRAG";
+    private static final String CHAT_ID = "1";
+    private TextView mMessageOutputTextView;
+    private EditText mMessageInputEditText;
+    private String mEmail;
+    private String mSendUrl;
+
+
+    public ChatFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        SharedPreferences prefs =
+                getActivity().getSharedPreferences(
+                        getString(R.string.keys_shared_prefs),
+                        Context.MODE_PRIVATE);
+        if (prefs.contains(getString(R.string.keys_prefs_email))) {
+            mEmail = prefs.getString(getString(R.string.keys_prefs_email), "");
+        } else {
+            throw new IllegalStateException("No EMAIL in prefs!");
+        }
+        //We will use this url every time the user hits send. Let's only build it once, ya?
+        mSendUrl = new Uri.Builder()
+                .scheme("https")
+                .appendPath(getString(R.string.ep_base_url))
+                .appendPath(getString(R.string.ep_messaging_base))
+                .appendPath(getString(R.string.ep_messaging_send))
+                .build()
+                .toString();
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootLayout = inflater.inflate(R.layout.fragment_chat, container, false);
+        mMessageOutputTextView = rootLayout.findViewById(R.id.text_chat_message_display);
+        mMessageInputEditText = rootLayout.findViewById(R.id.edit_chat_message_input);
+        rootLayout.findViewById(R.id.button_chat_send).setOnClickListener(this::handleSendClick);
+        return rootLayout;
+    }
+
+    private void handleSendClick(final View theButton) {
+        String msg = mMessageInputEditText.getText().toString();
+        JSONObject messageJson = new JSONObject();
+        try {
+            messageJson.put("email", mEmail);
+            messageJson.put("message", msg);
+            messageJson.put("chatId", CHAT_ID);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        new SendPostAsyncTask.Builder(mSendUrl, messageJson)
+                .onPostExecute(this::endOfSendMsgTask)
+                .onCancelled(error -> Log.e(TAG, error))
+                .build().execute();
+    }
+    private void endOfSendMsgTask(final String result) {
+        try {
+            //This is the result from the web service
+            JSONObject res = new JSONObject(result);
+            if(res.has("success") && res.getBoolean("success")) {
+                //The web service got our message. Time to clear out the input EditText
+                mMessageInputEditText.setText("");
+                //its up to you to decide if you want to send the message to the output here
+                //or wait for the message to come back from the web service.
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
